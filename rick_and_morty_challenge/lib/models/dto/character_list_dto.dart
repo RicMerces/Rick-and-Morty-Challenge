6e643051097574248c51ad29bdd0306c865aa521
RicMerces <ricardoliveira0703@gmail.com>
2022-10-04 18:48:15 -0300

    Adicionando client+ dto

---
+++ rick_and_morty_challenge/lib/models/dto/character_list_dto.dart
@@ -0,0 +1,137 @@
+class CharacterListDto {
+  Info? info;
+  List<Results>? results;
+
+  CharacterListDto({this.info, this.results});
+
+  CharacterListDto.fromJson(Map<String, dynamic> json) {
+    info = json['info'] != null ? Info.fromJson(json['info']) : null;
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (info != null) {
+      data['info'] = info!.toJson();
+    }
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Info {
+  int? count;
+  int? pages;
+  String? next;
+  String? prev;
+
+  Info({this.count, this.pages, this.next, this.prev});
+
+  Info.fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    pages = json['pages'];
+    next = json['next'];
+    prev = json['prev'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['count'] = count;
+    data['pages'] = pages;
+    data['next'] = next;
+    data['prev'] = prev;
+    return data;
+  }
+}
+
+class Results {
+  int? id;
+  String? name;
+  String? status;
+  String? species;
+  String? type;
+  String? gender;
+  Origin? origin;
+  Origin? location;
+  String? image;
+  List<String>? episode;
+  String? url;
+  String? created;
+
+  Results(
+      {this.id,
+      this.name,
+      this.status,
+      this.species,
+      this.type,
+      this.gender,
+      this.origin,
+      this.location,
+      this.image,
+      this.episode,
+      this.url,
+      this.created});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    status = json['status'];
+    species = json['species'];
+    type = json['type'];
+    gender = json['gender'];
+    origin = json['origin'] != null ? Origin.fromJson(json['origin']) : null;
+    location =
+        json['location'] != null ? Origin.fromJson(json['location']) : null;
+    image = json['image'];
+    episode = json['episode'].cast<String>();
+    url = json['url'];
+    created = json['created'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['status'] = status;
+    data['species'] = species;
+    data['type'] = type;
+    data['gender'] = gender;
+    if (origin != null) {
+      data['origin'] = origin!.toJson();
+    }
+    if (location != null) {
+      data['location'] = location!.toJson();
+    }
+    data['image'] = image;
+    data['episode'] = episode;
+    data['url'] = url;
+    data['created'] = created;
+    return data;
+  }
+}
+
+class Origin {
+  String? name;
+  String? url;
+
+  Origin({this.name, this.url});
+
+  Origin.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['url'] = url;
+    return data;
+  }
+}
--- rick_and_morty_challenge/lib/screens/first_page.dart
+++ rick_and_morty_challenge/lib/screens/first_page.dart
@@ -7,31 +7,35 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff181A20),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Image.asset('assets/image1.png'),
-          ),
-          const SizedBox(
-            height: 200,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Color(0xffA2CF2A),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 150),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(
+              child: Image.asset('assets/image1.png'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xffA2CF2A),
+                minimumSize: const Size(230, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
+              onPressed: () {},
               child: const Text(
                 'Iniciar',
                 style: TextStyle(
                   color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
